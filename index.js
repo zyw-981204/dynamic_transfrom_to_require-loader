d4567b01e42cfc88c956c69e75fd9bed13701d28
zyw-981204 <qq1364525128@163.com>
2022-08-14 23:47:33 +0800

    feat: 可以读取loader中的 options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const loderUtils = require('loader-utils');
 module.exports = function (source) {
   const reg = /import\(([.|\s]*)\)/g;
-
+  const options = loderUtils.getOptions(this);
   const content = source.replaceAll(reg, (...args) => {
     return `require(${args[1]})`;
   });
